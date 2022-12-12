10dadf784378fc5f975222ea77d5ba35c1c88082
Arjun <arjun@infracloud.io>
2022-12-12 10:02:20 +0530

    deleting repo

--- istio/tetrate/Deploy.sh
+++ istio/tetrate/Deploy.sh
@@ -1,9 +1,30 @@
 #!/bin/bash 
 istioctl operator init  
 istioctl apply -f profile.yaml -y
-kubectl get iop -A 
+
+STATUS=`kubectl get iop -A  | awk '{prinet $4}'`
+if [ "${STATUS}" == 'HEALTHY' ]; then
+    echo -e "\n 🔹The pod status is healthy now 🎉"
+    break
+fi
+echo -e "\n 🔹Waiting the istiod status  to become 1/1 ⏰"
+
+sleep 4
+
+echo -e "\n  🔹The pod status must be  healthy now 🎉"
+echo -e "\n"
 kubectl get po -n istio-system
-kubectl label ns default istio-injection=enabled
+
+echo -e "\n 🔹Injecting label to default namespace 💉"
+echo -e "\n"
+kubectl label ns default istio-injection=enabled 
+
 kubectl get ns -L istio-injection 
 
-kubectl apply -f ./addone 
+
+echo -e "\n 🔹Applying addone 🛒"
+
+kubectl apply -f ./addone > /dev/null
+
+echo -e "\n 🔹Installation completed 🎌"
+echo -e "\n"
