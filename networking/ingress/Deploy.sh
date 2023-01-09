aad8e9eb9d276a295bb72d7da1ef16b8d4dd1783
arjunavinfra <118735091+arjunavinfra@users.noreply.github.com>
2023-01-09 17:31:36 +0530

    Update Deploy.sh

--- networking/ingress/Deploy.sh
+++ networking/ingress/Deploy.sh
@@ -2,7 +2,7 @@
 kubectl apply -f ingress-operator.yaml
 
 echo "Waithing the pod to be in ready state....!!!!!!"
-kubectl wait --namespace ingress-nginx \
-  --for=condition=ready pod \
-  --selector=app.kubernetes.io/component=controller \
-  --timeout=150s
+# kubectl wait --namespace ingress-nginx \
+#   --for=condition=ready pod \
+#   --selector=app.kubernetes.io/component=controller \
+#   --timeout=150s
